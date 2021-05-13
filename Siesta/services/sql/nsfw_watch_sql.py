fedf4cddae4c51d90066056cbe2213ce6bbd010c
Hayaka Ryu <muhnurfauzan73@gmail.com>
2021-05-13 20:38:27 +0800

    Update nsfw_watch_sql.py

--- Siesta/services/sql/nsfw_watch_sql.py
+++ Siesta/services/sql/nsfw_watch_sql.py
@@ -11,7 +11,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from DaisyX.services.sql import BASE, SESSION
+from Siesta.services.sql import BASE, SESSION
 from sqlalchemy import Column, String
 
 
