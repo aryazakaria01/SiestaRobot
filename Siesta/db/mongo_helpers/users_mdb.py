4fb25d632056a11342d38741c6fc4360c2e7b440
Hayaka Ryu <muhnurfauzan73@gmail.com>
2021-05-13 20:22:36 +0800

    Update users_mdb.py

--- Siesta/db/mongo_helpers/users_mdb.py
+++ Siesta/db/mongo_helpers/users_mdb.py
@@ -1,5 +1,5 @@
 import pymongo
-from DaisyX.config import get_str_key
+from Siesta.config import get_str_key
 
 MONGO2 = get_str_key("FILTERS_MONGO", None)
 MONGO = get_str_key("MONGO_URI", required=True)
