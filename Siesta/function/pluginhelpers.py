fb7ccbfb085f04cfa3b043f95fdc2fcf5bdb9b14
Hayaka Ryu <muhnurfauzan73@gmail.com>
2021-05-13 20:34:50 +0800

    Update pluginhelpers.py

--- Siesta/function/pluginhelpers.py
+++ Siesta/function/pluginhelpers.py
@@ -8,8 +8,8 @@ from functools import wraps
 from typing import Callable, Coroutine, Dict, List, Tuple, Union
 
 import aiohttp
-from DaisyX import OWNER_ID, SUPPORT_CHAT
-from DaisyX.services.pyrogram import pbot
+from Siesta import OWNER_ID, SUPPORT_CHAT
+from Siesta.services.pyrogram import pbot
 from PIL import Image
 from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
