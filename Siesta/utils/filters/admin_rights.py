09a6c941ee189282f1411257560f7f176a2514bd
Hayaka Ryu <muhnurfauzan73@gmail.com>
2021-05-13 20:50:02 +0800

    Update admin_rights.py

--- Siesta/utils/filters/admin_rights.py
+++ Siesta/utils/filters/admin_rights.py
@@ -18,9 +18,9 @@ from dataclasses import dataclass
 from aiogram.dispatcher.filters import Filter
 from aiogram.types.callback_query import CallbackQuery
 from aiogram.utils.exceptions import BadRequest
-from DaisyX import BOT_ID, dp
-from DaisyX.modules.utils.language import get_strings
-from DaisyX.modules.utils.user_details import check_admin_rights
+from Siesta import BOT_ID, dp
+from Siesta.modules.utils.language import get_strings
+from Siesta.modules.utils.user_details import check_admin_rights
 
 
 @dataclass
