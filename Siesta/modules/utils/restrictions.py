71ffd30a808cd851f8aa3730ab164b11b0308ea6
Hayaka Ryu <muhnurfauzan73@gmail.com>
2021-05-15 20:06:06 +0800

    Update restrictions.py

--- Siesta/modules/utils/restrictions.py
+++ Siesta/modules/utils/restrictions.py
@@ -15,7 +15,7 @@
 
 from aiogram.types.chat_permissions import ChatPermissions
 from aiogram.utils.exceptions import BadRequest, MigrateToChat, Unauthorized
-from DaisyX import bot
+from Siesta import bot
 
 
 async def ban_user(chat_id, user_id, until_date=None):
