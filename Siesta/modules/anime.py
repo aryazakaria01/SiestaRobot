7c36ab1c67df51c242b38bfb80e91aa5066d77c9
Hayaka Ryu <muhnurfauzan73@gmail.com>
2021-05-16 06:38:09 +0800

    Update anime.py

--- Siesta/modules/anime.py
+++ Siesta/modules/anime.py
@@ -24,7 +24,7 @@ import bs4
 import jikanpy
 import requests
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from DaisyX.decorator import register
+from Siesta.decorator import register
 
 from .utils.anime import (
     airing_query,
