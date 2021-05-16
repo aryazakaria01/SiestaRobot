7334fb1795333edc44b5161cb972445d31a8ae85
Hayaka Ryu <muhnurfauzan73@gmail.com>
2021-05-16 09:02:57 +0800

    Update tts-stt.py

--- Siesta/modules/tts-stt.py
+++ Siesta/modules/tts-stt.py
@@ -21,9 +21,9 @@ import os
 from datetime import datetime
 
 import requests
-from DaisyX.config import get_str_key
-from DaisyX.services.events import register
-from DaisyX.services.telethon import tbot
+from Siesta.config import get_str_key
+from Siesta.services.events import register
+from Siesta.services.telethon import tbot
 from gtts import gTTS, gTTSError
 from telethon.tl import functions, types
 
